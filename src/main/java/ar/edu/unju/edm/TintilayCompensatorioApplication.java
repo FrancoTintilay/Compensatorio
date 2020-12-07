99b624cc2d2e2554a087cb4a967e24971e0b6df3
admin <admin@LAPTOP-0PALGUM3>
2020-12-07 20:59:26 -0300

    Inicializacion del Proyecto

---
+++ src/main/java/ar/edu/unju/edm/TintilayCompensatorioApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.edm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TintilayCompensatorioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TintilayCompensatorioApplication.class, args);
+	}
+
+}
